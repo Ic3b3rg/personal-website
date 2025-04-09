97c33ba5a2abc54cbb9f0e5c6cc3d6251e4eaa33
Silvio Ceccarini <silvioceccarini@Mac.home>
2025-04-09 20:03:41 +0200

    feat: Improve build update flow

--- check-build.js
+++ check-build.js
@@ -1,6 +1,9 @@
 import fs from "fs";
 import crypto from "crypto";
-import { exec } from "child_process";
+import { exec as callbackExec } from "child_process";
+import { promisify } from "util";
+
+const exec = promisify(callbackExec);
 
 const API_URL = `https://api.github.com/users/Ic3b3rg/events`;
 const HASH_FILE = "./api-data.hash";
@@ -10,6 +13,7 @@ function saveLog(log) {
   const timestamp = new Intl.DateTimeFormat("it-IT", {
     dateStyle: "short",
     timeStyle: "medium",
+    timeZone: "Europe/Rome",
   }).format(logDate);
   fs.appendFileSync("build.log", `[${timestamp}] ${log}\n`, "utf8");
 }
@@ -39,36 +43,96 @@ function getHash() {
     saveLog(
       "Nessun hash precedente trovato, build eseguita per la prima volta.",
     );
+    return null;
+  }
+}
+
+async function checkEdit() {
+  try {
+    const { stdout } = await exec("git pull");
+    if (stdout.includes("Already up to date.")) {
+      saveLog("Nessuna modifica trovata nel repository.");
+      return false;
+    } else {
+      saveLog("Modifiche trovate nel repository.");
+      return true;
+    }
+  } catch (error) {
+    saveLog(`Errore durante git pull: ${error}`);
+    return false; // In caso di errore, considera il repository come modificato (o gestisci l'errore diversamente)
+  }
+}
+
+async function installDependencies() {
+  saveLog("Installazione delle dipendenze...");
+  try {
+    const { stdout, stderr } = await exec("npm install");
+    saveLog(stdout);
+    if (stderr) {
+      saveLog(stderr);
+      console.error(stderr);
+    }
+  } catch (error) {
+    saveLog(`Errore durante l'installazione delle dipendenze: ${error}`);
+  }
+}
+
+async function buildProject() {
+  saveLog("I dati API sono cambiati. Esecuzione della build...");
+  try {
+    const { stdout, stderr } = await exec("npm run build");
+    saveLog(stdout);
+    if (stderr) {
+      saveLog(stderr);
+    }
+    saveLog("Build completata con successo.");
+  } catch (error) {
+    console.error(`Errore durante la build: ${error}`);
+    saveLog(JSON.stringify(error));
+  }
+}
+
+async function reloadNginx() {
+  saveLog("Ricaricamento di Nginx...");
+  try {
+    const { stdout, stderr } = await exec("sudo systemctl reload nginx");
+    saveLog(stdout);
+    if (stderr) {
+      saveLog(stderr);
+      console.error(stderr);
+    }
+    return stdout;
+  } catch (error) {
+    console.error(`Errore durante il reload di Nginx: ${error}`);
+    saveLog(JSON.stringify(error));
+    return null;
   }
 }
 
 async function checkAndBuild() {
   createHashFile();
-
+  const oldHash = getHash();
   try {
+    const isRepoEdit = await checkEdit();
+    if (isRepoEdit) {
+      await installDependencies();
+    }
     const data = await fetchEvents();
     const newHash = computeHash(data);
-    const oldHash = getHash();
-
-    if (newHash === oldHash) {
-      saveLog("I dati API non sono cambiati. Build saltata.");
+    fs.writeFileSync(HASH_FILE, newHash, "utf8");
+    const isEventsEdit = newHash === oldHash;
+    if (isEventsEdit || !isRepoEdit) {
+      saveLog(
+        "I dati API e della repository non sono cambiati. Build saltata.",
+      );
     } else {
-      fs.writeFileSync(HASH_FILE, newHash, "utf8");
-      saveLog("I dati API sono cambiati. Esecuzione della build...");
-      exec("npm run build", (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Errore durante la build: ${error}`);
-          return;
-        }
-        saveLog(stdout);
-        if (stderr) {
-          saveLog(stderr);
-          console.error(stderr);
-        }
-      });
+      await buildProject();
+      await reloadNginx();
     }
   } catch (error) {
-    saveLog(`Errore nella lettura delle API: ${JSON.stringify(error)}`);
+    saveLog(
+      `Errore nella lettura delle API o nel controllo del repository: ${JSON.stringify(error)}`,
+    );
   }
 }
 
